ae86a9f471930787bdf4bf1f0a73c6319365c2f4
oxhide1 <55039774+oxhide1@users.noreply.github.com>
2024-03-11 06:04:52 +0800

    Add project files.

---
+++ Bluelight Exercise/FactorChecker.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluelight_Exercise
+{
+    public class FactorChecker
+    {
+        // Constructor
+        public FactorChecker (int start, int end, int divisor)
+        {
+            this.Start = start;
+            this.End = end;
+            this.Divisor = divisor;
+
+            Range = GetRange();
+        }
+
+        // Member declarations
+        public int Start;
+        public int End;
+        public int Divisor;
+        public List<int> Range;
+
+        // Method to create int collection given start and end ints
+        private List<int> GetRange()
+        {
+            List<int> result = new List<int>();
+            int current;
+
+            if (Start < End)
+            {
+                current = Start;
+                while (current <= End)
+                {
+                    result.Add(current);
+                    current++;
+                }
+            }
+            else
+            {
+                result.Add(Start);
+            }
+
+            return result;
+        }
+
+        // ================================================================
+        // Method 1: Using modulus division
+        // ================================================================
+        public void Method1 ()
+        {
+            List<int> result = new List<int>();
+            int remainder;
+
+            Console.WriteLine("\nUsing modulus:");
+
+            foreach (int dividend in Range)
+            {
+                // *** Math is here ***
+                remainder = dividend % Divisor;
+
+                // Append and print results
+                if (remainder == 0)
+                {
+                    result.Add(dividend);
+                    Console.WriteLine($"{dividend} mod {Divisor} = {remainder}; adding to result");
+                }
+                else
+                {
+                    Console.WriteLine($"{dividend} mod {Divisor} = {remainder}; not valid");
+                }
+            }
+
+            Console.WriteLine("\nFinal result:");
+            Console.WriteLine(string.Join(", ", result.ToArray()));
+        }
+
+        // ================================================================
+        // Method 2: Using non-modulus division
+        // ================================================================
+        public void Method2 ()
+        {
+            List<int> result = new List<int>();
+            int product;
+
+            Console.WriteLine("\nUsing integer division:");
+
+            foreach (int dividend in Range)
+            {
+                // *** Math is here ***
+                product = (dividend / Divisor) * Divisor;
+
+                // Append and print results
+                if (product == dividend)
+                {
+                    result.Add(dividend);
+                    Console.WriteLine($"( {dividend} / {Divisor} ) * {Divisor} = {product}; adding to result");
+                }
+                else
+                {
+                    Console.WriteLine($"( {dividend} / {Divisor} ) * {Divisor} = {product}; not valid");
+                }
+            }
+
+            Console.WriteLine("\nFinal result:");
+            Console.WriteLine(string.Join(", ", result.ToArray()));
+        }
+
+        // ================================================================
+        // Method 3: Using no division at all
+        // ================================================================
+        public void Method3()
+        {
+            List<int> result = new List<int>();
+            int current = 0;
+
+            Console.WriteLine("\nUsing no division; getting multiples of divisor within range");
+
+            // *** Math is here ***
+            // Increment divisor until it reaches the start value
+            while (current < Start)
+            {
+                current += Divisor;
+            }
+            // Start adding multiples to results until it exceeds the end value
+            while (current <= End)
+            {
+                result.Add(current);
+                current += Divisor;
+            }
+
+            Console.WriteLine("\nFinal result:");
+            Console.WriteLine(string.Join(", ", result.ToArray()));
+        }
+    }
+}
---
+++ Bluelight Exercise/Program.cs
@@ -0,0 +1,74 @@
+﻿// ================================================================
+// See FactorChecker.cs for method logic
+// ================================================================
+
+using Bluelight_Exercise;
+
+// Declarations
+int start = 0;
+int end = 0;
+int divisor = 0;
+
+int input;
+
+// Header text
+Console.WriteLine("Simple factor checker");
+Console.WriteLine("Written by Angelo Loveranes for Bluelight CRM interview");
+Console.WriteLine("================================================================");
+
+// Get start
+Console.WriteLine("Please enter a starting integer (inclusive):");
+start = Convert.ToInt32(Console.ReadLine());
+
+// Get end
+Console.WriteLine("Please enter an ending integer (inclusive):");
+end = Convert.ToInt32(Console.ReadLine());
+// Validate end
+while (end <= start)
+{
+    Console.WriteLine("End value must be greater than start, please enter a different end value:");
+    end = Convert.ToInt32(Console.ReadLine());
+}
+
+// Get divisor
+Console.WriteLine("Please enter a divisor:");
+divisor = Convert.ToInt32(Console.ReadLine());
+// Validate divisor
+while (divisor <= 0)
+{
+    Console.WriteLine("Divisor cannot be zero or negative; please enter a different divisor:");
+    divisor = Convert.ToInt32(Console.ReadLine());
+}
+
+// Print methods
+Console.WriteLine("");
+Console.WriteLine("================================================================");
+Console.WriteLine("Method 1 - via modulus division:");
+Console.WriteLine("Method 2 - via non-modulus division:");
+Console.WriteLine("Method 3 - without division:");
+
+// Prompt method selection
+Console.WriteLine("\nSelect a method:");
+input = Convert.ToInt32(Console.ReadLine());
+
+// Initialize class then execute selected method
+Console.WriteLine("================================================================");
+FactorChecker factorChecker = new FactorChecker(start, end, divisor);
+//Console.WriteLine($"\nFound range: {string.Join(", ", factorChecker.Range.ToArray())}");
+switch(input)
+{
+    case 1:
+        factorChecker.Method1();
+        break;
+    case 2:
+        factorChecker.Method2();
+        break;
+    case 3:
+        factorChecker.Method3();
+        break;
+    default:
+        Console.WriteLine("Input not recognized; press any key to exit...");
+        Console.ReadKey();
+        break;
+}
+Console.ReadKey();
